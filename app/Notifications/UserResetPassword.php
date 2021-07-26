36d92a54e24b6af526216f05db668ae5b6f28c6e
Ivan <canarivan495@gmail.com>
2021-07-26 14:31:07 -0500

    (EMAIL) RESET PASSWORD

---
+++ app/Notifications/UserResetPassword.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserResetPassword extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Restablecer contraseña Aspralnues')
+            ->greeting('¡Hola que tal!')
+            ->line('Hemos recibido una petición para restablecer tu contraseña.')
+            ->action('Restablecer contraseña', url('/password/reset/'.$this->token))
+            ->line('Si tu no haz echo esta petición comunicate con el administrador')
+            ->salutation('Gracias.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
